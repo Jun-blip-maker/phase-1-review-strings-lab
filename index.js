985f1e738aecda74b7fd849e4384315429fd246d
khadijun@DESKTOP <kardikhadija1@gmail.com>
2025-02-27 22:01:50 +0300

    phase-1-review-strings-lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+//const currentUser = "Grace Hopper";
+//const welcomeMessage = "Welcome to Flatbook, ";
+//const currentUser = "Grace Hopper";
+
+//const welcomeMessage = "Welcome to Flatbook, " + currentUser;
+//const currentUser = "Grace Hopper";
+
+//const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const currentUser = "Grace Hopper";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//const shortGreeting = "Welcome, ";
+//const shortGreeting = `Welcome, ${currentUser}`;
+//currentUser.slice(0, 1);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
